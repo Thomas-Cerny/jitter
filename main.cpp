ed0175138937851c18b32fb64e2759acc8134b3f
22ACernyT <22acernyt@student.panska.cz>
2025-01-22 00:18:58 +0100

    moved on from z-score method, fixed errors, simplified and commented code, final version

--- main.cpp
+++ main.cpp
@@ -5,37 +5,55 @@
 #include <algorithm>
 #include <iostream>
 #include <numeric>
-#include <cstdint>
 
 struct Data {
-    std::vector<float> wanted_vtr;
-    uint16_t wanted_size;
-}
+    std::vector<float> wanted_vtr;  //elements will be altered, used for interval calculation
+    std::vector<float> wanted_alt;  //will be sorted in intervals
+    unsigned short wanted_size;
+};
 
+//store amount of numbers of each interval in intervals_count[] and interval outliers in intervals[]
 struct Interval {
-    uint16_t interval_count[12];
+    unsigned short interval_count[12] = { 0 };
     float intervals[11];
-}
+    float tolerance;
+};
 
-float calculateMean(const Data& data);
-float calculateStdDev(const Data& data, float mean);
-void removeOutliers(const Data& data, float mean, float std_dev, const Interval& interval);
-void calculateIntervals(const std::vector<float>& result, const Interval& interval);
-float calculateDispersion(const Data& data);
-float calculateJitter(const std::vector<float>& wanted_vtr, const Data& data);
-void sortIntervals(const std::vector<float>& result, const Data& data, const Interval& interval);
-void printToFile(const Interval& interval);
+float calculateMean(const Data data);
+float calculateDispersion(const Data data);
+float calculateJitter(const Data data);
+void calculateIntervals(const Data data, Interval& interval);
+void removeOutliers(Data& data, Interval& interval);
+void manRemove(Data& data, Interval& interval);
+void sortIntervals(const Data data, Interval& interval);
+void consoleOut(const Data data, const Interval interval);
+double setPrecision(float val);
 
 int main()
 {
-    //read data from input.txt and store the wanted data in wanted_vtr
+    //read data from input.txt
     Data data;
 
     std::regex wanted_rgx(R"(time=(\d+(\.\d+)?))");
 
     std::ifstream input("input.txt");
 
-        //check if the data was loaded
+    std::string file_data((std::istreambuf_iterator<char>(input)), std::istreambuf_iterator<char>());
+
+    //store wanted data matching wanted_rgx in wanted_vtr
+    auto wanted_begin = std::sregex_iterator(file_data.begin(), file_data.end(), wanted_rgx);
+    auto wanted_end = std::sregex_iterator();
+
+    for (; wanted_begin != wanted_end; wanted_begin++)
+    {
+        std::string match_time = (*wanted_begin)[1].str();
+
+        data.wanted_vtr.push_back(stof(match_time));     //store wanted data in wanted_vtr as type float
+    }
+
+    input.close();
+
+    //check if data was loaded
     if (data.wanted_vtr.empty())
     {
         std::cout << "Couldn't read data from file" << std::endl;
@@ -43,147 +61,47 @@ int main()
         return 1;
     }
 
-    std::string file_data((std::istreambuf_iterator<char>(input)), std::istreambuf_iterator<char>());
+    data.wanted_size = data.wanted_vtr.size();    //calculate size of wanted_vtr, max value is +65535, this is the maximum amount of ping values
 
-    auto wanted_begin = std::sregex_iterator(file_data.begin(), file_data.end(), wanted_rgx);
-    auto wanted_end = std::sregex_iterator();
-
-    for ( ; wanted_begin != wanted_end; wanted_begin++)
-    {
-        std::string match_time = (*wanted_begin) [1].str();
-
-        data.wanted_vtr.push_back(stod(match_time));     //stores the wanted data in wanted_vtr as type float
-    }
-
-    input.close();
-
-    data.wanted_size = data.wanted_vtr.size();    //calculates the size of wanted_vtr, max value is +65535, this is the maximum amount of ping values
-
-    float mean = calculateMean(data);    //calculates the mean of wanted_vtr
-    float std_dev = calculateStdDev(data, mean); //calculates the standard deviation of wanted_vtr
-    float dispersion = calculateDispersion(data.wanted_vtr);   //calculates the dispersion of wanted_vtr
-    float jitter = calculateJitter(data);   //calculates the packet-to-packet jitter of wanted_vtr
-
-    std::cout << "mean: " << mean << std::endl;
-    std::cout << "std_dev: " << std_dev << std::endl;
-    std::cout << "dispersion: " << dispersion << std::endl;
-    std::cout << "jitter: " << jitter << std::endl;
+    float mean = calculateMean(data);    //calculate mean of wanted_vtr
+    float dispersion = calculateDispersion(data);   //calculate dispersion of wanted_vtr
+    float jitter = calculateJitter(data);   //calculate packet-to-packet jitter of wanted_vtr
 
     Interval interval;
 
-    interval.intervals[11];
-    interval.interval_count[12] = {0};  //stores the amount of numbers in each interval
+    data.wanted_alt = data.wanted_vtr;      //copy elements of wanted_vtr to wanted_alt
+    std::sort(data.wanted_alt.begin(), data.wanted_alt.end());
 
-    //debug
-    for (const auto& i : wanted_vtr)
-        std::cout << i << " ";
+    calculateIntervals(data, interval);     //calculate intervals
 
-    std::cout << std::endl;
+    removeOutliers(data, interval);     //remove outliers, recalculate intervals, sort intervals
 
-    std::cout << "pocet hodnot:" << data.wanted_vtr.size() << std::endl;
-    //
+    consoleOut(data, interval);     //print sorted values to console
 
-    for (size_t i = 0; i < 11; i++)
-        interval.intervals[i] = i * 10;
+    manRemove(data, interval);      //manually remove values, recalculate intervals, sort intervals
 
-    //debug
-    for (size_t i = 0; i < 11; i++)
-        std::cout << interval.intervals[i] << " ";
-    //
+    consoleOut(data, interval);     //print final sorted values to console
 
-    removeOutliers(data, mean, std_dev, interval_count, intervals);
-
-    //print results to file output.txt
-    printToFile(interval_count, intervals);
-
-    //print results to console
-    for (size_t i = 0; i < 11; i++)
-        std::cout << data.intervals[i] << " - " << data.intervals[i + 1] << ": " << data.interval_count[i] << std::endl << std::endl;
-
-    std::cout << "Mean: " << mean << std::endl << std::endl;
-
-    std::cout << "Packet-to-packet jitter: " << jitter << std::endl << std::endl;
-
-    std::cout << "Dispersion: " << dispersion << std::endl << std::endl;
+    //print mean, dispersion and jitter to console
+    std::cout << "Mean: " << setPrecision(mean) << std::endl;
+    std::cout << "Dispersion: " << setPrecision(dispersion) << std::endl;
+    std::cout << "Jitter: " << jitter << std::endl;
 }
 
-//calculate mean of wanted_vtr - CHECK FOR LOGICAL CALCULATION ERROR
-float calculateMean(const Data& data)
+//calculate mean of wanted_vtr
+float calculateMean(const Data data)
 {
-    return std::accumulate(data.wanted_vtr.begin(), data.wanted_vtr.end(), 0) / data.wanted_size;
-}
-
-//calculate standard deviation of wanted_vtr - CHECK FOR LOGICAL CALCULATION ERROR
-float calculateStdDev(const Data& data, float mean)
-{
-    float sum = 0;
-
-    for (auto& i : data.wanted_vtr)
-        sum += std::pow(i - mean, 2);
-
-    return std::sqrt(sum / data.wanted_size);
-}
-
-//remove outliers from wanted_vtr, calculate intervals and sort wanted_vtr into the intervals
-void removeOutliers(const Data& data, float mean, float std_dev, const Interval& interval)
-{
-    std::vector<float> result;
-    std::vector<float> outliers;
-
-    for (auto& i : data.wanted_vtr)
-        if (i < mean + 1.5 * std_dev && i > mean - 1.5 * std_dev)   //if the number is within 1.5 standard deviations from the mean, it is not an outlier
-            result.push_back(i);
-        else
-        {
-            outliers.push_back(i);
-
-            std::cout << "Deleted number: " << i << std::endl;  
-        }
-
-    std::cout << "============================================================" << std::endl;
-
-    calculateIntervals(result, interval);
-
-    //debug
-    for (size_t i = 0; i < 11; i++)
-        interval.intervals[i] = 0;
-
-    for (size_t i = 0; i < 12; i++)
-        interval.interval_count[i] = 0;
-    //
-
-    for (const auto& i : outliers)
-        result.push_back(i);
-
-    sortIntervals(result, interval, data);
-
-    //debug
-    for (size_t i = 0; i < 13; i++)
-        std::cout << interval.interval_count[i] << " ";
-    //
-}
-
-//calculates the outliers for 10 intervals - CHECK FOR LOGICAL CALCULATION ERROR
-void calculateIntervals(const std::vector<float>& result, const Interval& interval)
-{
-    float min_outlier = *std::min_element(result.begin(), result.end());
-    float max_outlier = *std::max_element(result.begin(), result.end());
-
-    interval.intervals[0] = min_outlier;
-    interval.intervals[10] = max_outlier;
-
-    for (size_t i = 1; i < 11; i++)
-        interval.intervals[i] = interval.intervals[0] + i * (interval.intervals[10] - interval.intervals[0]) / 10;
+    return std::accumulate(data.wanted_vtr.begin(), data.wanted_vtr.end(), 0.0f) / data.wanted_size;
 }
 
 //calculate dispersion of wanted_vtr
-float calculateDispersion(const Data& data)
+float calculateDispersion(const Data data)
 {
-    return *std::max_element(data.wanted_vtr.begin(), wanted_vtr.end()) - *std::min_element(wanted_vtr.begin(), wanted_vtr.end());
+    return *std::max_element(data.wanted_vtr.begin(), data.wanted_vtr.end()) - *std::min_element(data.wanted_vtr.begin(), data.wanted_vtr.end());
 }
 
 //calculate packet-to-packet jitter of wanted_vtr
-float calculateJitter(const std::vector<float>& wanted_vtr, const Data& data)
+float calculateJitter(const Data data)
 {
     float sum = 0;
 
@@ -193,23 +111,97 @@ float calculateJitter(const std::vector<float>& wanted_vtr, const Data& data)
     return sum / (data.wanted_size - 1);
 }
 
-//sorting wanted_vtr into the already defined intervals - CHECK FOR LOGICAL ERROR
-void sortIntervals(const std::vector<float>& result, const Data& data, const Interval& interval)
+//calculates the outliers for 10 intervals
+void calculateIntervals(const Data data, Interval& interval)
 {
-    for (size_t i = 0; i < data.wanted_size; i++)
-        for (size_t j = 0; j < 12; j++)
+    //calculates the biggest and smallest interval outliers
+    float min_outlier = *std::min_element(data.wanted_alt.begin(), data.wanted_alt.end());
+    float max_outlier = *std::max_element(data.wanted_alt.begin(), data.wanted_alt.end());
+
+    interval.intervals[0] = min_outlier;
+    interval.intervals[10] = max_outlier;
+
+    //calculates the medial interval outliers from min_outlier and max_outlier (a bit inaccurate)
+    for (size_t i = 1; i < 10; i++)
+        interval.intervals[i] = interval.intervals[0] + i * ((interval.intervals[10] - interval.intervals[0]) / 10);
+}
+
+//automaticaly remove wanted_vtr elements larger than tolerance and store in wanted_alt, console output
+void removeOutliers(Data& data, Interval& interval)
+{
+    while (data.wanted_alt.size() >= 4)     //continue cycle if there are 4+ elements in wanted_alt
+    {
+        size_t size = data.wanted_alt.size();   //ids of last 4 elements in wanted_alt
+
+        //check whether each difference of pairs of last 4 elements of wanted_alt is bigger than set tolerance
+        if ((data.wanted_alt[size - 1] - data.wanted_alt[size - 2] > interval.tolerance) || (data.wanted_alt[size - 2] - data.wanted_alt[size - 3] > interval.tolerance) || (data.wanted_alt[size - 3] - data.wanted_alt[size - 4] > interval.tolerance))
         {
-            if (result[i] < data.intervals[0])   //if the number is smaller than the first interval, it is stored in the first interval
+            std::cout << "Deleted number: " << data.wanted_alt.back() << std::endl;
+
+            data.wanted_alt.pop_back();     //if condition is met, delete last element of wanted_alt
+        }
+        else
+            break;
+    }
+
+    std::cout << std::endl;
+
+    calculateIntervals(data, interval);
+
+    sortIntervals(data, interval);
+}
+
+//choice to manually remove elements from front and back of wanted_alt
+void manRemove(Data& data, Interval& interval)
+{
+    std::cout << "Delete more numbers? [y/n]" << std::endl << std::endl;
+
+    char del_char;
+    std::cin >> del_char;
+
+    if (del_char == 'y')        //deletion if del_char is 'y'
+    {
+        std::cout << std::endl << "How many from front?" << std::endl << std::endl;
+
+        short del_front;
+        std::cin >> del_front;      //value must be 0 or greater, 0 means no erasion
+
+        std::cout << std::endl << "How many from back?" << std::endl << std::endl;
+
+        short del_back;
+        std::cin >> del_back;       //value must be 0 or greater, 0 means no erasion
+
+        if (del_front > 0)
+            for (size_t i = 0; i < del_front; i++)
+                data.wanted_alt.erase(data.wanted_alt.begin());   //erase elements from front of wanted_alt if condition is met
+
+        if (del_back > 0)
+            for (size_t i = 0; i < del_back; i++)
+                data.wanted_alt.pop_back();         //erase elements from back of wanted_alt if condition is met
+    }
+
+    calculateIntervals(data, interval);
+
+    sortIntervals(data, interval);
+}
+
+//sorting wanted_vtr into the already defined intervals
+void sortIntervals(const Data data, Interval& interval)
+{
+    for (size_t i = 0; i < data.wanted_size; i++)   //sorting wanted_vtr, wanted_size = wanted_vtr.size()
+        for (size_t j = 0; j < 11; j++)             //loops for 12 intervals
+        {
+            if (data.wanted_vtr[i] < interval.intervals[0])     //if number is smaller than first interval
             {
                 interval.interval_count[0]++;
                 break;
             }
-            else if (result[i] >= interval.intervals[j] && result[i] < interval.intervals[j + 1])  //if the number is within the interval, it is stored in the interval
+            else if (data.wanted_vtr[i] >= interval.intervals[j] && data.wanted_vtr[i] < interval.intervals[j + 1])  //if number is within indexed interval
             {
                 interval.interval_count[j]++;
                 break;
             }
-            else if (result[i] >= interval.intervals[11])    //if the number is bigger than the last interval, it is stored in the last interval
+            else if (data.wanted_vtr[i] >= interval.intervals[10])    //if number is greater than last interval
             {
                 interval.interval_count[11]++;
                 break;
@@ -217,12 +209,19 @@ void sortIntervals(const std::vector<float>& result, const Data& data, const Int
         }
 }
 
-void printToFile(const Interval& interval)
+//print sorted results to console, number printed with 2 decimal place precision
+void consoleOut(const Data data, const Interval interval)
 {
-    std::ofstream output("output.txt");
+    std::cout << "0 - " << setPrecision(interval.intervals[0]) << ": " << interval.interval_count[0] << std::endl;
 
-    for (size_t i = 0; i < 11; i++)
-        output << interval.intervals[i] << " - " << interval.intervals[i + 1] << ": " << interval.interval_count[i] << std::endl;
+    for (size_t i = 1; i < 11; i++)
+        std::cout << setPrecision(interval.intervals[i - 1]) << " - " << setPrecision(interval.intervals[i]) << ": " << interval.interval_count[i] << std::endl;
 
-    output.close();
+    std::cout << setPrecision(interval.intervals[10]) << "+ : " << interval.interval_count[11] << std::endl << std::endl;
+}
+
+//set precision to 2 decimal places
+double setPrecision(float val)
+{
+    return std::round(val * 100.) / 100.;
 }
